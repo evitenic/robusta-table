4808ab02305d07e33b2d3b9e7c4d5044013ce002
evitenic <41444348+evitenic@users.noreply.github.com>
2025-06-17 08:20:39 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,7 @@ namespace Evitenic\RobustaTable\Tests;
 
 use BladeUI\Heroicons\BladeHeroiconsServiceProvider;
 use BladeUI\Icons\BladeIconsServiceProvider;
+use Evitenic\RobustaTable\RobustaTableServiceProvider;
 use Filament\Actions\ActionsServiceProvider;
 use Filament\FilamentServiceProvider;
 use Filament\Forms\FormsServiceProvider;
@@ -16,7 +17,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use Livewire\LivewireServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 use RyanChandler\BladeCaptureDirective\BladeCaptureDirectiveServiceProvider;
-use Evitenic\RobustaTable\RobustaTableServiceProvider;
 
 class TestCase extends Orchestra
 {
