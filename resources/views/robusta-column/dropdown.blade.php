5fb12a7ff814e7d84f80d5c35948e0e905df2253
evitenic <ahmads1704@gmail.com>
2025-06-23 11:59:58 +0700

    feat: toggle column + reorder column

--- resources/views/robusta-column/dropdown.blade.php
+++ resources/views/robusta-column/dropdown.blade.php
@@ -1,34 +1,46 @@
-@props([
-    'columns',
-    'maxHeight' => null,
-    'triggerAction',
-    'width' => 'xs',
-])
-
-<x-filament::dropdown
-    :max-height="$maxHeight"
-    placement="bottom-end"
-    shift
-    :width="$width"
-    wire:key="{{ $this->getId() }}.robusta-manage-column"
-    {{ $attributes->class(['fi-ta-col-toggle']) }}
->
+@props(['columns', 'maxHeight' => null, 'triggerAction', 'width' => 'xs'])
+@php
+    $canReorderColumns = $this->getTable()->isReorderableColumns();
+@endphp
+<x-filament::dropdown :max-height="$maxHeight" placement="bottom-end" shift :width="$width"
+    wire:key="{{ $this->getId() }}.robusta-manage-column" {{ $attributes->class(['fi-ta-col-toggle']) }}>
     <x-slot name="trigger">
         {{ $triggerAction }}
     </x-slot>
 
-    <div class="grid gap-y-4 p-6">
-        <h4
-            class="text-base font-semibold leading-6 text-gray-950 dark:text-white"
-        >
+    <div x-data="{ isLoading: false }" class="p-6">
+        <h4 class="text-base font-semibold leading-6 text-gray-950 dark:text-white">
             {{ __('robusta-table::robusta-table.columns') }}
         </h4>
+        <div x-robusta-sortable="isLoading"
+            x-on:sorted="
+                isLoading = true;
+                $wire.call('updateColumnOrder', $event.detail).finally(() => isLoading = false)
+            "
+            class="grid gap-y-4 mt-4">
+            @foreach ($columns as $key => $column)
+                @php
+                    $isColumnVisible = $this->getToggleColumnState($column->getName());
+                @endphp
+                <div x-sortable-item="{{ $column->getName() }}"
+                    :class="['flex gap-x-3 items-center', isLoading ? 'opacity-50' : '']"
+                    wire:key="{{ $column->getName() }}">
+                    @if ($column->isToggleable())
+                        <x-filament::icon-button x-bind:disabled="isLoading" color="primary"
+                            icon="{{ $isColumnVisible ? config('robusta-table.icons.column-visible') : config('robusta-table.icons.column-hidden') }}"
+                            wire:click="toggleColumnVisibility('{{ $column->getName() }}')" />
+                    @else
+                        <div class="h-5 w-5"></div>
+                    @endif
 
-        @foreach ($columns as $column)
-            <div class="flex gap-1 items-center">
-                <x-filament::icon-button icon="{{config('robusta-table.icons.column-visible')}}" />
-                {{ $column->getLabel() }}
-            </div>
-        @endforeach
+                    <span
+                        class="text-sm font-medium leading-6 text-gray-950 dark:text-white flex-1">{{ $column->getLabel() }}</span>
+                    <x-filament::icon
+                        class="h-5 w-5 text-primary-600 dark:text-primary-500 robusta-sortable-handle cursor-move"
+                        icon="{{ config('robusta-table.icons.order-column') }}" />
+                </div>
+            @endforeach
+        </div>
     </div>
+
 </x-filament::dropdown>
