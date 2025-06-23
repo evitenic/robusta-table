836463208c74178956129a5f704907d6c5e95811
evitenic <ahmads1704@gmail.com>
2025-06-23 12:01:35 +0700

    feat: HasToggleColumn trait for managing toggleable columns

---
+++ src/Concerns/Table/HasToggleColumn.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Evitenic\RobustaTable\Concerns\Table;
+
+use Closure;
+
+trait HasToggleColumn
+{
+    protected bool | Closure $persistsToggledColumns = false;
+
+    public function persistsToggledColumns(bool $persists = true): static
+    {
+        $this->persistsToggledColumns = $persists;
+
+        return $this;
+    }
+
+    public function isPersistingToggledColumns(): bool
+    {
+        return $this->evaluate($this->persistsToggledColumns);
+    }
+
+    /**
+     * disable default toggleable columns feature
+     */
+    public function hasToggleableColumns(): bool
+    {
+        return false;
+    }
+}
