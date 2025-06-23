c6615328c11674b58bc763852af5b852641f9c84
evitenic <ahmads1704@gmail.com>
2025-06-23 12:01:08 +0700

    feat: add HasReorderColumns trait for column reordering functionality

---
+++ src/Concerns/Table/HasReorderColumns.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Evitenic\RobustaTable\Concerns\Table;
+
+use Closure;
+
+trait HasReorderColumns
+{
+    protected bool | Closure $isReorderableColumns = false;
+
+    protected bool | Closure $persistsReorderedColumns = false;
+
+    public function reorderableColumns(bool | Closure $isReorderableColumns = true): static
+    {
+        $this->isReorderableColumns = $isReorderableColumns;
+
+        return $this;
+    }
+
+    public function isReorderableColumns(): bool
+    {
+        return $this->evaluate($this->isReorderableColumns);
+    }
+
+    public function persistsReorderedColumns(bool $persists = true): static
+    {
+        $this->persistsReorderedColumns = $persists;
+
+        return $this;
+    }
+
+    public function isPersistingReorderedColumns(): bool
+    {
+        return $this->evaluate($this->persistsReorderedColumns);
+    }
+}
