94915372dcf519d0283a836165f40c38822c7262
evitenic <ahmads1704@gmail.com>
2025-06-23 13:21:39 +0700

    feat: implement RobustaTableStore class for store management

---
+++ src/Store/RobustaTableStore.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Evitenic\RobustaTable\Store;
+
+use Evitenic\RobustaTable\Contracts\Store;
+use Evitenic\RobustaTable\Store\SessionStore;
+
+class RobustaTableStore
+{
+    protected static ?self $instance = null;
+
+    protected Store $store;
+
+    protected function __construct()
+    {
+        $configStore = config('robusta-table.store_driver');
+
+        if (is_null($configStore)) {
+            throw new \RuntimeException('Store configuration is not set.');
+        }
+
+        if ($configStore === 'session') {
+            $this->store = new SessionStore();
+        } elseif (class_exists($configStore)) {
+            $this->store = app($configStore); // Bisa inject via container
+        } else {
+            throw new \RuntimeException("Store class {$configStore} does not exist.");
+        }
+    }
+
+    public static function getInstance(): self
+    {
+        if (!static::$instance) {
+            static::$instance = new static();
+        }
+
+        return static::$instance;
+    }
+
+    public function db(): Store
+    {
+        return $this->store;
+    }
+}
