7ce2a5ec2654f7622e15a6ca0b609f4781988f67
Tinnnooo <tinnofabregas01@gmail.com>
2025-08-19 09:15:44 +0000

    chore: remove unused file

--- resources/views/wrapper.blade.php
+++
@@ -1,5 +0,0 @@
-<div x-load x-load-css="[@js(\Filament\Support\Facades\FilamentAsset::getStyleHref('robusta-table-styles', 'evitenic/robusta-table'))]"
-    x-load-src="{{ \Filament\Support\Facades\FilamentAsset::getAlpineComponentSrc('robusta-table', 'evitenic/robusta-table') }}"
-    x-data="initRobustaTable({ resizedColumn: @js($this->getResizeableColumnsConfig()) })" @if ($this->getTable()->isLoaded()) x-init="registerPlugin()" @endif>
-    {{ $slot }}
-</div>
