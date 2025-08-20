82004b33b1ad3ebce78b92b172c298fb49c97357
Tinnnooo <tinnofabregas01@gmail.com>
2025-08-20 07:05:42 +0000

    update initialize resize column

--- resources/js/robusta-table.js
+++ resources/js/robusta-table.js
@@ -26,6 +26,8 @@ export default function filamentRobustaTable({columns, resizedConfig}){
 
             Livewire.hook("morph.updated", () => {
                 this.initialized = false;
+
+                this.checkAndInitialize();
             })
 
             this.element = this.$el;
